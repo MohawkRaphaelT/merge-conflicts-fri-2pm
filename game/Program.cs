b2e23f3d52c449163f78d8440915752332bea059
MohawkRaphaelT <150467892+MohawkRaphaelT@users.noreply.github.com>
2023-11-17 14:26:47 -0500

    Add C# project

---
+++ game/Program.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+
+namespace game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

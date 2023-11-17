1c8a2b6eb35c7fccf1e390f6b1a5920fb200c732
Neil-McCuaig <Neil_McCuaig@bell.net>
2023-11-17 15:16:41 -0500

    Create CUBE.cs

---
+++ game/CUBE.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+
+namespace game
+{
+    internal class CUBE
+    {
+        public void Draw()
+        {
+            Raylib.DrawCircle(200, 200, 100, Color.PURPLE);
+        }
+    }
+}

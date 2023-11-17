c16fb59e1e051cf5155439acaf61cd1889ed6538
lukyos <kyle200402@gmail.com>
2023-11-17 15:27:10 -0500

    Create square.cs

---
+++ game/square.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+
+namespace game
+{
+    internal class Square
+    {
+        public void Draw()
+        {
+            Raylib.DrawRectangle(1, 1, 50, 50, Color.BEIGE);
+        }
+    }
+}
